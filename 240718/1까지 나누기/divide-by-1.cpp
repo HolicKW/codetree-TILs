c7a9a44ba71d75a32b3697ae98d5a6c79f8c8b59
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-18 14:57:04 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/1까지 나누기/divide-by-1.cpp
+++ 240718/1까지 나누기/divide-by-1.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    int count = 1;
+    int count = 0;
     cin >> n;
     int i = 0;
     while(1){
         i++;
-        n/= i;
-        if(n/i ==0){
+        count++;
+        if(n/i <= 1){
             break;
         }
-        count++;
+        n/=i;
     }
     cout <<count;
     return 0;
