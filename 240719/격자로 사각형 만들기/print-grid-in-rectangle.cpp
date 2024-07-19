c1c93b35fd850901dbab747686554be7c2f7832d
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-19 22:58:24 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ 240719/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -5,7 +5,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int arr[n][n] = {1,1,1,1,1};
+    int arr[n][n];
+
+    for(int i = 0; i<n; i++){
+        arr[0][i] = 1;
+    }
     for(int i = 1; i<n;i++){
         arr[i][0] = 1;
     }
