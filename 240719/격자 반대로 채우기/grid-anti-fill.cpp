70fcb63e0fdebd12720331e14811b64c6ae2cda8
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-19 18:51:15 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240719/격자 반대로 채우기/grid-anti-fill.cpp
@@ -8,18 +8,21 @@ int main() {
     int num = 1;
     cin >> n;
     int arr[n][n];
+    bool up = true;
     for(int j = n-1; j>=0; j--){
-        if(j%2 == 1){
+        if(up){
             for(int i=n-1;i>=0;i--){
                 arr[i][j] = num;
                 num++;
             }
+            up = false;
         }
         else{
             for(int i=0; i<n;i++){
                 arr[i][j] = num;
                 num++;
             }
+            up = true;
         }
         
     }
