2306f51794a6b215bda6be6a9c94e998adbc249c
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-21 02:27:22 +0900

    Title: 문자열 범위 출력하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240721/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240721/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -8,7 +8,7 @@ int main() {
     int n,cnt(0);
     cin >> str;
     cin >> n;
-    if (n<str.length()){
+    if (n<=str.length()){
         for(int i = str.length()-1; i>0;i--){
             if(cnt == n){
                 break;
