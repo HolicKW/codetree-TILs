06451bbc5218593fcacbca2cfd436ffffac9b196
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-21 22:30:40 +0900

    Title: 특정 문자의 등장 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240721/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
+++ 240721/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     string str;
-    int cnt1,cnt2;
+    int cnt1(0),cnt2(0);
     cin >> str;
-    if(str.find("ee") != string::npos){
-        cnt1++;
+    for(int i = 1; i<str.length();i++){
+        if(str[i-1] == 'e' && str[i] == 'e'){
+            cnt1++;
+        }
     }
     
     if(str.find("eb") != string::npos){
