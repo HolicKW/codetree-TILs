a98eedb02e6a9f8477cd39eb3627702c6d2786a2
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-21 23:49:09 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/부분문자열 위치 구하기/find-location-of-substring.cpp
+++ 240721/부분문자열 위치 구하기/find-location-of-substring.cpp
@@ -3,23 +3,11 @@
 using namespace std;
 
 int main() {
-    string inputStr, targetStr;
-    cin >> inputStr >> targetStr;
+    string a,b;
+    cin >> a >> b;
 
-    int inputLen = inputStr.length();
-    int targetLen = targetStr.length();
-    int count = 0;
-
-    // 입력 문자열에서 목적 문자열의 발생 횟수를 계산
-    for (int i = 0; i <= inputLen - targetLen; i++) {
-        if (inputStr.substr(i, targetLen) == targetStr) {
-            count++;
-        }
-    }
-
-    // 결과 출력
-    if(count !=0){
-    cout << count << endl;
+    if(a.find(b) != string::npos){
+        cout << a.find(b);
     }
     else{
         cout << "-1";
