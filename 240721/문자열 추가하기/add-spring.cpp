578b27693429d43dbaef2cdb347b02538ca0b4dd
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-21 16:19:51 +0900

    Title: 문자열 추가하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1,str2("Hello");
+    cin >> str1;
+    str1 += str2;
+    cout <<str1;
+    return 0;
+}
