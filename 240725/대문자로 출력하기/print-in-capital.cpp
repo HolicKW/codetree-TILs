c8591139768008ee9511c9228c37e229f08600f8
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-25 17:40:55 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/대문자로 출력하기/print-in-capital.cpp
+++ 240725/대문자로 출력하기/print-in-capital.cpp
@@ -7,11 +7,11 @@ int main() {
     string str;
     cin >> str;
     for(int i = 0; i<str.length(); i++){
-        if(str[i] >'A' && str[i] <'Z'){
+        if(str[i] >='A' && str[i] <='Z'){
             cout << str[i];
             continue;
         }
-        if(str[i]>'a' && str[i] <'z'){
+        if(str[i]>='a' && str[i] <='z'){
             str[i] = char(str[i] - 'a' + 'A');
             cout << str[i];
         }
