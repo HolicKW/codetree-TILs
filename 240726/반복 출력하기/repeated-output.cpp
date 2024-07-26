11bb5fa473fefde9e379d80f6e37804972999441
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-26 21:16:57 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void pr(int n){
+    for(int i = 0; i<n;i++){
+        cout << "12345^&*()_";
+        cout << endl;
+    }
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    pr(n);
+    return 0;
+}
