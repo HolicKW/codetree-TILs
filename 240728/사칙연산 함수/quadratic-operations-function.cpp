e08ff30647264230d3114819a6063a9de9541d15
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-28 01:07:43 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int sum(int a,int b){
+    return a+b;
+}
+int sub(int a,int b){
+    return a-b;
+}
+int divi(int a,int b){
+    return a/b;
+}
+int mul(int a,int b){
+    return a*b;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char sign;
+    int result;
+    cin >> a >> sign >> b;
+    if(sign == '+'){
+        result = sum(a,b);
+    }
+    else if(sign == '-'){
+        result = sub(a,b);
+    }
+    else if(sign == '/'){
+        result = divi(a,b);
+    }
+    else if(sign == '*'){
+        result = mul(a,b);
+    }
+
+    cout << a << ' ' << sign << ' ' << b << " = " << result;
+    return 0;
+}
