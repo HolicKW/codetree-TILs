e3d0670edefc4a9f1828da99fe6fedf702c0f7a8
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-27 18:04:42 +0900

    Title: 함수를 이용한 소수 판별, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240727/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 bool isprime(int n){
-    if(n == 1 || n == 2){
-        return true;
+    if(n == 1){
+        return false;
     }
     for(int i = 2; i<n;i++){
         if(n%i == 0)
