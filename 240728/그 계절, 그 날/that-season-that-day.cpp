169e151c36801ae2a07934159a44b6422467c6c8
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-28 19:03:05 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+bool istrue(int n){
+    if(n%4 == 0){
+        if(n % 100 == 0){
+            if(n%400 == 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y,m,d;
+    cin >> y >> m >> d;
+
+    int M[12] ={31,28,31,30,31,30,31,31,30,31,30,31};
+    if(istrue){
+        M[1] = 29;
+    }
+
+    if(M[m] < d){
+        cout << "-1";
+        return 0;
+    }
+    
+    if(m>=3 && m<=5){
+        cout <<"Spring";
+    }
+    else if(m>=6 && m<=8){
+        cout <<"Summer";
+    }
+    else if(m>=9 && m<=11){
+        cout <<"Fall";
+    }
+    else{
+        cout <<"Winter"; 
+    }
+    return 0;
+}
