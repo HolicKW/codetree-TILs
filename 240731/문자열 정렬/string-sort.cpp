831268ad0b653712f32e34f05043970d937b5d8e
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-07-31 18:13:39 +0900

    Title: 문자열 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/문자열 정렬/string-sort.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    sort(str.begin(),str.end());
+    cout << str;
+    return 0;
+}
