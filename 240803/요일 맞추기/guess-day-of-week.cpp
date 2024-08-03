15d9e7d37c0c7ee340da6a8b5e98372ec0ecdec3
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-03 22:37:19 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int su(int m1, int d1){
+    int month[13] ={0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total = 0;
+    for(int i = 0; i<m1; i++){
+        total += month[i];
+    }
+    total += d1;
+    return total;
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,m2,d1,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    int a = su(m1,d1);
+    int b = su(m2,d2);
+    int result = (b-a+365)%7;
+    string week[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    int month[13] ={0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    cout << week[result];
+    return 0;
+}
