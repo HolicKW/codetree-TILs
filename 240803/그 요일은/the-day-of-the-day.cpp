bb3989b9c936712f9fa35b3deee3864b8d9e35c5
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-03 23:31:21 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int su(int m1, int d1){
+    int month[13] ={0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total = 0;
+    for(int i = 0; i<m1; i++){
+        total += month[i];
+    }
+    total += d1;
+    return total;
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,d1,m2,d2;
+    string start;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> start;
+    int a = su(m1,d1);
+    int b = su(m2,d2);
+    int result = b-a;
+    int cnt = 0;
+    int index=0;
+    string week[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    for(int i = 0; i<7; i++){
+        if(week[i] == start){
+            index = i;
+            break;
+        }
+    }
+    result -= index;
+    cnt++;
+    while(result >= 7){
+        result -= 7;
+        cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
