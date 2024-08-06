ef25d5b85d7e00073494f0120ae5817934c30963
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-06 23:10:00 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[2000] = {0,};
+    int loc = 1000;
+
+    for(int i = 0; i<n; i++){
+        int a;
+        char b;
+        cin >> a >> b;
+        if(b == 'L'){
+            for(int i = 0; i<a; i++){
+                arr[loc]++;
+                loc--;
+            }
+        }
+        else{
+            for(int i =0; i<a; i++){
+                arr[loc]++;
+                loc++;
+            }
+        }
+    }
+    int cnt(0);
+    for(int i = 0; i<2000; i++){
+        if(arr[i] >= 2){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    
+    return 0;
+}
