af9521901af16ebf8c90804f90267d3e8ab537a3
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-07 01:33:27 +0900

    Title: 흰검 칠하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/흰검 칠하기/painting-white-black.cpp
+++ 240807/흰검 칠하기/painting-white-black.cpp
@@ -1,73 +1,82 @@
 #include <iostream>
 using namespace std;
 
-class Tile {
-public:
-    char color;
-    int white_count;
-    int black_count;
+class tile{
+    public:
+        char color;
+        int cnt1;
+        int cnt2;
 
-    Tile() {
-        color = '0'; // 초기 색상 없음
-        white_count = 0;
-        black_count = 0;
-    }
-
-    void paint(char new_color) {
-        if (color == 'G') return; // 회색 타일은 더 이상 색상이 바뀌지 않음
-
-        if (new_color == 'W') {
-            white_count++;
-        } else if (new_color == 'B') {
-            black_count++;
+        tile(){
+            cnt1 = 0;
+            cnt2 = 0;
         }
 
-        if (white_count >= 2 && black_count >= 2) {
-            color = 'G'; // 회색으로 변경
-        } else {
-            color = new_color; // 새로운 색상으로 변경
-        }
-    }
 };
-
 int main() {
+    // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    Tile tiles[20000]; // 충분히 큰 배열 할당
-    int loc = 10000; // 초기 위치는 배열의 중앙
-
-    for (int i = 0; i < n; i++) {
+    tile tiles[2000];
+    int loc = 1000;
+    int bl = 0;
+    int wh = 0;
+    int gr = 0;
+    for(int i = 0; i<n; i++){
         int a;
         char b;
         cin >> a >> b;
-        if (b == 'L') {
-            for (int j = 0; j < a; j++) {
-                tiles[loc].paint('W');
-                if (j != a - 1) loc--; // 마지막 칸 이동 제외
+        if(b == 'L'){
+            for(int j = 0; j<a; j++){
+                tiles[loc].cnt1++;
+                if(tiles[loc].color == 'G'){
+                    break;
+                }
+                else{
+                    if(tiles[loc].cnt1 == 2 && tiles[loc].cnt2 == 2){
+                        tiles[loc].color = 'G';
+                    }
+                    else
+                        tiles[loc].color = 'W'; 
+                }
+                if(j != a-1){
+                    loc--;
+                }
             }
-        } else if (b == 'R') {
-            for (int j = 0; j < a; j++) {
-                tiles[loc].paint('B');
-                if (j != a - 1) loc++; // 마지막 칸 이동 제외
+
+
+        }
+        else if(b == 'R'){
+             for(int j = 0; j<a; j++){
+                tiles[loc].cnt2++;
+                if(tiles[loc].color == 'G'){
+                    break;
+                }
+                else{
+                    if(tiles[loc].cnt1 == 2 && tiles[loc].cnt2 == 2){
+                        tiles[loc].color = 'G';
+                    }
+                    else
+                        tiles[loc].color = 'B';
+                }
+                if(j != a-1){
+                    loc++;
+                }    
             }
         }
     }
-
-    int white_count = 0;
-    int black_count = 0;
-    int gray_count = 0;
-
-    for (int i = 0; i < 20000; i++) {
-        if (tiles[i].color == 'W') {
-            white_count++;
-        } else if (tiles[i].color == 'B') {
-            black_count++;
-        } else if (tiles[i].color == 'G') {
-            gray_count++;
+    for(int i = 0; i<2000; i++){
+        if(tiles[i].color == 'W'){
+            wh++;
+        }
+        else if(tiles[i].color == 'B'){
+            bl++;
+        }
+        else if(tiles[i].color == 'G'){
+            gr++;
         }
     }
-
-    cout << white_count << ' ' << black_count << ' ' << gray_count << endl;
+    cout << wh << ' ' << bl << ' ' << gr;
 
     return 0;
 }
