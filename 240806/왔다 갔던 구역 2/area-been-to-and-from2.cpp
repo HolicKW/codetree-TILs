c53bacf53d3e8bf2f8673cb22514ceaeeb12b795
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-06 23:28:51 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240806/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int arr[2000] = {0,};
-    int loc = 1000;
 
-    for(int i = 0; i<n; i++){
+    vector<int> positions(2000, 0);  // 큰 배열을 사용하여 양방향 이동을 처리
+    int loc = 1000;  // 시작 위치를 배열 중간으로 설정
+
+    // 주어진 명령에 따라 위치 이동 및 빈도수 계산
+    for (int i = 0; i < n; ++i) {
         int a;
-        char b;
-        cin >> a >> b;
-        if(b == 'L'){
-            for(int j = 0; j<a; j++){
+        char dir;
+        cin >> a >> dir;
+        
+        if (dir == 'L') {
+            for (int j = 0; j < a; ++j) {
                 loc--;
-                arr[loc]++;
+                positions[loc]++;
             }
-        }
-        else{
-            for(int j =0; j<a; j++){
+        } else {
+            for (int j = 0; j < a; ++j) {
                 loc++;
-                arr[loc]++;
-                
+                positions[loc]++;
             }
         }
     }
-    int cnt(0);
-    for(int i = 0; i<2000; i++){
-        if(arr[i] >= 2){
-            cnt++;
+
+    int count = 0;  // 빈도수가 2 이상인 위치의 수를 셀 변수
+    for (int i = 0; i < 2000; ++i) {
+        if (positions[i] >= 2) {
+            count++;
         }
     }
-    cout << cnt;
-    
+
+    cout << count << endl;  // 결과 출력
+
     return 0;
 }
