8a1e50b81a0aa8c610ddc7bb2a79ebfbfb23dd65
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-09 16:59:42 +0900

    Title: 벌금은 누구에게, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/벌금은 누구에게/who-will-pay.cpp
+++ 240809/벌금은 누구에게/who-will-pay.cpp
@@ -5,12 +5,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n,m,k;
     cin >> n >> m >>k;
-    int arr[n] = {0,};
+    int arr[101] = {0,};
+    int a;
     for(int i = 0; i<m; i++){
-        int a;
         cin >> a;
         arr[a]++;
-    
+        //cout << arr[a] << endl;
     }
 
     for(int i = 1; i<=n; i++){
