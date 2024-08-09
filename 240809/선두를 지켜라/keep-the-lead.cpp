dbe9c8193c7490d74085a7d9f96ab40b892cc72b
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-09 20:47:48 +0900

    Title: 선두를 지켜라, Time: 10ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240809/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+#define max_r 1000001
+
+int cmp(int a[], int b[], int len){
+    int cnt(0);
+    for(int i = 1; i<len-1; i++){
+        if((a[i]>=b[i] && a[i+1] < b[i+1]) || (a[i]<=b[i] && a[i+1] > b[i+1])){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin >> n >> m;
+    int a[max_r] = {0,};
+    int b[max_r] = {0,};
+    int count_a = 1;
+    int count_b = 1;
+    for(int i =0; i<n;i++){
+        int v,t;
+        cin >> v >> t;
+        while(t--){
+            a[count_a] = a[count_a -1]+v;
+            count_a++;
+        }
+        
+    }
+
+    for(int i =0; i<m;i++){
+        int v,t;
+        cin >> v >> t;
+        while(t--){
+            b[count_b] = b[count_b -1]+v;
+            count_b++;
+        }
+        
+    }
+    
+    cout << cmp(a,b,count_a);
+
+    return 0;
+}
