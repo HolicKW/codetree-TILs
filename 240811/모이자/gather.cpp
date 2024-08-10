94651947834f4601f70af330f0ed353f97428cc5
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-08-11 04:20:47 +0900

    Title: 모이자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/모이자/gather.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n;i++){
+        cin >> arr[i];
+    }
+    int min(100);
+    for(int i = 0 ; i<n; i++){
+        int dis(0);
+        for(int j = 0; j<n; j++){
+            dis += arr[j] * abs((j-i));
+        }
+        if(min > dis){
+            min = dis;
+        }
+    }
+    cout << min;
+    return 0;
+}
