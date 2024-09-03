5425eccc51a201bffc3cc473c21d4a8212d2dfb0
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-09-03 13:13:26 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240903/체크판위에서 2/on-the-checkboard-2.cpp
@@ -4,35 +4,30 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int r,c;
+    int result(0);
     cin >> r >> c;
     char arr[r][c];
-    bool checked[r][c] = {0,};
     for(int i = 0; i<r; i++){
         for(int j = 0; j<c; j++){
             cin >>arr[i][j];
         }
     }
 
-    char color = arr[0][0];
-    int lo_r = 0;
-    int lo_c = 0;
-    int result(0);
-    for(int i = 0; i<r-1; i++){
-        int cnt(0);
-        for(int j = 0; j<c-1; j++){
-            if(arr[i][j] != color && (lo_r < i && lo_c < j)){
-                if(cnt <=2){
-                    cnt++;
-                    lo_r = i;
-                    lo_c = j;
-                    color = arr[i][j];
-                }
-                else{
-                    result++;
+    char start = arr[0][0];
+    char end = arr[r-1][c-1];
+    
+    for(int i = 1; i<r; i++){
+        for(int j = 1; j<c; j++){
+            for(int k = i+1; k<r-1; k++){
+                for(int l = j+1; l<c-1; l++){
+                    if(arr[i][j] != start && arr[k][l] != arr[i][j] && arr[k][l] != end){
+                        result++;
+                    }
                 }
             }
         }
     }
+
     cout << result;
     return 0;
 }
