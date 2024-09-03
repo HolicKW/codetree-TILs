3b1eaece5d62be7435b9447b0361514390625f26
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-09-03 20:51:51 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 240903/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -3,27 +3,37 @@
 #include <algorithm>
 #include <cstdlib>
 
+#define MAX_N 100
+
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n;
-    int x[100] = 0;
-    int y[100] = 0;
-    for(int i = 0; i<n; i++){
-        cin >> x[i] >> y[i];
-    }
-    int ans = INT_MAX;
-    int dis;
-    for(int i = 1; i<n-1; i++){
-        int prev = 0;
-        for(int j = 1; j<n;j++){
-            if(j == i) continue; 
-            dis += abs(x[prev] - x[j]) + abs(y[prev] - y[j]);
-        }
+int n;
+int x[MAX_N], y[MAX_N];
 
-        ans = min(ans, dis);
-    }
+int main() {
+    // 변수 선언 및 입력
+    cin >> n;
+	for(int i = 0; i < n; i++)
+		cin >> x[i] >> y[i];
+	
+	// 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+	int ans = INT_MAX;
+    for(int i = 1; i < n - 1; i++) {
+		// 거리를 구합니다.
+		int dist = 0;
+		int prev_idx = 0;
+    	for(int j = 1; j < n; j++) {
+			if(j == i) continue;
+        	dist += abs(x[prev_idx] - x[j]) + abs(y[prev_idx] - y[j]);
+			prev_idx = j;
+		}
+		
+		// 가능한 거리 중 최솟값을 구해줍니다.
+		ans = min(ans, dist);
+	}
+    
+    // 출력
     cout << ans;
+	
     return 0;
 }
