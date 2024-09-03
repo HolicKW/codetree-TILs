16f3ceb58f07c988ef6a7e0a06bbd186491da33c
HolicKW <87267361+HolicKW@users.noreply.github.com>
2024-09-03 19:37:52 +0900

    Title: 이상한 진수 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240903/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    cin >> s;
+    bool check = false;
+    for(int i = 0; i<s.length(); i++){
+        if(!check){
+            if(s[i] == '0'){
+                s[i] = '0' + '1' - s[i];
+                check = true;
+            }
+        }
+        else if(!check && i == s.length()-1){
+            for(int i = s.length()-1; i>=0;i--){
+                if(s[i] == '1'){
+                    s[i] = '0' + '1' - s[i];
+                    break;
+                }
+            }
+        }
+    }
+    int result(0);
+    for(int i = 0; i<s.length(); i++){
+        result = result * 2 + (s[i]-'0');
+    }
+    cout << result;
+    
+    return 0;
+}
