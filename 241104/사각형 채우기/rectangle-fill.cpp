cb56aa598ead2f68834edceaa27958e203693766
함대영 <87267361+HolicKW@users.noreply.github.com>
2024-11-04 19:52:49 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241104/사각형 채우기/rectangle-fill.cpp
+++ 241104/사각형 채우기/rectangle-fill.cpp
@@ -9,7 +9,7 @@ int main() {
     dp[1] = 1;
     dp[2]= 2;
     for(int i = 3; i<=n; i++){
-        dp[i] = dp[i-2] + dp[i-1];
+        dp[i] = dp[i-2] + dp[i-1]%10007;
     }
     cout << dp[n];
     return 0;
