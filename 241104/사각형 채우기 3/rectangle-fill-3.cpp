bda79ffb9246bf80fb73e0a2ac462d4aa6d1c3f4
함대영 <87267361+HolicKW@users.noreply.github.com>
2024-11-04 20:18:27 +0900

    Title: 사각형 채우기 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241104/사각형 채우기 3/rectangle-fill-3.cpp
+++ 241104/사각형 채우기 3/rectangle-fill-3.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
-    cin >> n;
+    cin >> n; // 입력받은 n
+
+    // dp 배열 초기화
     int dp[1001] = {0};
-    dp[1] = 2;
-    dp[2] = 7;
-    for(int i =3;i<=n;i++){
-        dp[i]= (2 * dp[i-1] + dp[i-2])%1000000007; 
+    dp[0] = 1; // 아무것도 채우지 않는 경우
+    dp[1] = 2; // 1 × 2를 채우는 경우
+    dp[2] = 7; // 2 × 2를 채우는 경우 (3가지 방법)
+
+    // dp 배열을 채워나갑니다.
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 1000000007;
     }
-    cout << dp[n];
+
+    // 결과 출력
+    cout << dp[n] << endl;
+
     return 0;
 }
