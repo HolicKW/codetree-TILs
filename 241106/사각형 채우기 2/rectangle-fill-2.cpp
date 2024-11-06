655b3d54e73ca7e83df836f36e7711eba93cd426
함대영 <87267361+HolicKW@users.noreply.github.com>
2024-11-06 22:18:23 +0900

    Title: 사각형 채우기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/사각형 채우기 2/rectangle-fill-2.cpp
+++ 241106/사각형 채우기 2/rectangle-fill-2.cpp
@@ -9,7 +9,7 @@ int main() {
     int n;
     cin >> n;
     for(int i = 3; i<=n; i++){
-        dp[i] = dp[i-1] + 2*dp[i-2];
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007;
     }
 
     cout << dp[n];
