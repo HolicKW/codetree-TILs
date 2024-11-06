de4a48daa04d5c48cdadb9472f687a1c95280a61
함대영 <87267361+HolicKW@users.noreply.github.com>
2024-11-06 22:10:50 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
+++ 241106/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int dp[20];
+    int dp[20] = {0,};
     dp[0] = 1;
     dp[1] = 1;
     dp[2] = 2;
@@ -12,8 +12,8 @@ int main() {
     cin >> n;
     for(int i = 4; i<=n; i++){
         int sum = 0;
-        for(int j = 1; j<n; j++){
-            sum += dp[j] * dp[i-j];
+        for(int j = 0; j<n; j++){
+            sum += dp[j] * dp[i-j-1];
         }
         dp[i] = sum;
     }
