1eb83c116d38d9bebbc8b9279d1fdf864f48c622
함대영 <87267361+HolicKW@users.noreply.github.com>
2024-11-14 15:56:55 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/기울어진 직사각형/slanted-rectangle.cpp
+++ 241114/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,48 +1,61 @@
 #include <iostream>
+#include <algorithm>
+
+#define MAX_N 20
+#define DIR_NUM 4
+
 using namespace std;
-int arr[20][20];
+
 int n;
-int dx[4] = {-1,-1,1,1};
-int dy[4] = {1,-1,-1,1};
+int grid[MAX_N][MAX_N];
 
-bool inRange(int x, int y){
-    return x>=0 && x<n && y>=0 && y<n;
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
-int findMax(int x, int y){
-    int sum = arr[x][y];
-    int i = 0;
-    int nx = x, ny = y;
-    for (int i = 0; i < 4; ++i) { 
-        while (true) {
-            nx += dx[i];
-            ny += dy[i];
 
-            if (inRange(nx, ny)) {
-                sum += arr[nx][ny];
-            } else {
-                break; // 범위를 벗어나면 해당 방향 종료
-            }
+int GetScore(int x, int y, int k, int l) {
+    int dx[DIR_NUM] = {-1, -1, 1, 1};
+    int dy[DIR_NUM] = {1, -1, -1, 1};
+    int move_num[DIR_NUM] = {k, l, k, l};
+    
+    int sum_of_nums = 0;
+
+    // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for(int d = 0; d < DIR_NUM; d++)
+        for(int q = 0; q < move_num[d]; q++) {
+            x += dx[d]; y += dy[d];
+                
+            // 기울어진 직사각형이 경계를 벗어나는 경우라면
+            // 불가능하다는 의미로 답이 갱신되지 않도록
+            // 0을 반환합니다.
+            if(!InRange(x, y))
+                return 0;
+			
+            sum_of_nums += grid[x][y];
         }
-    }
-    return sum;
+    
+    return sum_of_nums;
 }
+
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n;
-    int result = 0;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n;j++){
-            cin >> arr[i][j];
-        }
-    }
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    
+    int ans = 0;
+    
+    // (i, j)를 시작으로 1, 2, 3, 4 방향
+    // 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+    // 기울어진 직사각형을 잡아보는
+    // 완전탐색을 진행해봅니다.
+    for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			for(int k = 1; k < n; k++)
+				for(int l = 1; l < n; l++)
+                    ans = max(ans, GetScore(i, j, k, l));
 
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-           result = max(result,findMax(i,j));
-           
-        }
-    }
-
-    cout<<result;
+    cout << ans;
     return 0;
 }
