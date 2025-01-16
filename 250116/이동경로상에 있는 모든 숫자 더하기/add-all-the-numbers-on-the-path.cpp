8b1f8b1f6294ddc4a651128ee143c324f673b69a
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-16 20:25:06 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250116/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
+++ 250116/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int n,t;
+int n, t;
 string s;
 int arr[100][100];
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,-1,0,1};
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
 int dir = 0;
-bool inRange(int x, int y){
-    return x>=0 && x<n && y>=0 && y<n;
+bool inRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
 int main() {
     // Please write your code here.
     cin >> n >> t;
     cin >> s;
-    int x = n/2;
-    int y = n/2;
-    int result= 0;
-    for(int i = 0; i<n;i++){
-        for(int j = 0; j<n;j++){
-            cin >>arr[i][j];
+    int x = n / 2;
+    int y = n / 2;
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
     }
-    int nx = x+dx[dir];
-    int ny = y+dy[dir];
 
-    for(int i = 0; i<t;i++){
-        nx = x+dx[dir];
-        ny = y+dy[dir];
-        if(s[i] == 'L'){
-            dir = (dir+1)%4;
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+
+    for (int i = 0; i < t; i++) {
+        nx = x + dx[dir];
+        ny = y + dy[dir];
+
+        if (s[i] == 'L') {
+            dir = (dir + 1) % 4;
         }
-        else if(s[i] == 'R'){
-            dir = (dir+3) % 4;
+        else if (s[i] == 'R') {
+            dir = (dir + 3) % 4;
         }
-        else if(s[i] == 'F' && inRange(nx,ny)){
+        else if (s[i] == 'F' && inRange(nx, ny)) {
             result += arr[x][y];
-            x=nx;
-            y=ny;
+            x = nx;
+            y = ny;
+            if(i == t-1){
+                result += arr[x][y];
+            }
         }
+
     }
-    cout << result+ arr[nx][ny]; 
+
+    cout << result;
     return 0;
 }
