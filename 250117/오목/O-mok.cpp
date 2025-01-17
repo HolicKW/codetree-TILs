2e545e30e42f2d957909895c4d73190d997a42fd
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-17 13:35:42 +0900

    Title: 오목, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250117/오목/O-mok.cpp
+++ 250117/오목/O-mok.cpp
@@ -64,6 +64,26 @@ bool Win3(){
     }
     return false;
 }
+
+bool Win4(){
+    for(int i = 0; i<14;i++){
+        for(int j = 4; j<19;j++){
+            if(arr[i][j] == 1 && arr[i+1][j-1] == 1&& arr[i+2][j-2]== 1 && arr[i+3][j-3] == 1 && arr[i+4][j-4] == 1){
+                win = 1;
+                p.first = i+2;
+                p.second = j-2;
+                return true;
+            }
+            else if(arr[i][j] == 2 && arr[i+1][j-1] == 2&& arr[i+2][j-2]== 2 && arr[i+3][j-3] == 2 && arr[i+4][j-4] == 2){
+                win = 2;
+                p.first = i+2;
+                p.second = j-2;
+                return true;
+            }
+        }
+    }
+    return false;
+}
 int main() {
     // Please write your code here.
     for(int i = 0; i<19;i++){
@@ -74,7 +94,7 @@ int main() {
 
   
      
-    if(Win1()||Win2()||Win3()){
+    if(Win1()||Win2()||Win3()||Win4()){
         cout << win << endl;
         cout << p.first+1 << ' ' << p.second+1;
         return 0;
