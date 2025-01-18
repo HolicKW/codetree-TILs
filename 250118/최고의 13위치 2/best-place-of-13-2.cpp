dcc7ed76cc42e314cc132fa938c6caba49ad2eba
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-18 20:50:32 +0900

    Title: 최고의 13위치 2, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250118/최고의 13위치 2/best-place-of-13-2.cpp
+++ 250118/최고의 13위치 2/best-place-of-13-2.cpp
@@ -20,8 +20,8 @@ int main() {
     int cnt = 0;
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n-2;j++){
-            for(int k = i; k<n;k++){
-                for(int l = j; l<n-2; l++){
+            for(int k = 0; k<n;k++){
+                for(int l = 0; l<n-2; l++){
                     if(i == k && j==l) continue;
                     int max_cnt = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2];
                     cnt = max(cnt,max_cnt);
