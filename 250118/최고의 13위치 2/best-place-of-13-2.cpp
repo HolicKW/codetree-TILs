29b54dfb6b1ec025f6d2fe48e0b26f3da12f89f0
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-18 20:50:01 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250118/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[21][21];
+
+bool inRange(int x, int y){
+    return x>=0 && x<n && y>=0 && y<n;
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i = 0; i<n;i++){
+        for(int j = 0; j<n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    int cnt = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-2;j++){
+            for(int k = i; k<n;k++){
+                for(int l = j; l<n-2; l++){
+                    if(i == k && j==l) continue;
+                    int max_cnt = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2];
+                    cnt = max(cnt,max_cnt);
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
