80935e52298e1a952c479f5f35f3a4ef69753af4
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-20 19:47:18 +0900

    Title: G or H 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+char arr[101];
+int n;
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    int len = 0;
+    for(int i = 0; i<n;i++){
+        int num;
+        char alp;
+        cin >> num >> alp;
+        arr[num] = alp;
+        len = max(len,num);
+    }
+    int result = 0;
+    int x,y;
+    for(int i = 0; i<len+1;i++){
+        int count_G = 0;
+        int count_H = 0;
+        int sum = 0;
+        for(int j = i; j<len+1;j++){
+            if(arr[j] == 'G'){
+                count_G++;
+            }
+            else if (arr[j] == 'H'){
+                count_H++;
+            }
+            if((count_G == 0 && count_H>0) || (count_G > 0 && count_H == 0) || (count_G == count_H)){
+                sum = count_G+count_H;
+                if(sum > result){
+                    if(arr[i]!= 0 && arr[j]!=0){
+                        result = sum;
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+        
+    }
+
+    cout << y-x;
+    return 0;
+}
