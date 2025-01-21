693e284616874182042459562c9bf196cfaa62cd
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-21 16:28:47 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250121/개발자의 능력 3/ability-of-developer-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int result = 0;
+    int ans = INT_MAX;
+    int arr[6];
+    for(int i = 0; i<6 ;i++){
+        cin >> arr[i];
+        result += arr[i];
+    }
+
+    for(int i = 0; i<4; i++){
+        for(int j = i+1; j<6; j++){
+            for(int k =i+2;k<6;k++){
+                int tmp = arr[i] + arr[j] +arr[k];
+                int tmp2 = result - tmp;
+                ans = min(ans,abs(tmp2-tmp));
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
