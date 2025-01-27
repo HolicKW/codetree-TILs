e1add163f973bb0e8f776e1d23a1e21eeab40c2b
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-01-27 19:00:25 +0900

    Title: 알파벳과 사칙연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250127/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+char arr[201];
+string s;
+int num[6];
+int ans = 0;
+int cal(){
+    int result = num[arr[0] - 'a'];
+    for(int i = 1; i<s.length()-1; i++){
+        if(arr[i] == '+'){
+            result += num[arr[i+1]-'a'];
+        }
+        else if(arr[i] == '-'){
+            result -= num[arr[i+1]-'a'];
+        }
+        else if(arr[i] == '*'){
+            result *= num[arr[i+1]-'a'];
+        }
+    }
+    return result;
+}
+
+void choose(int count){
+    if(count == s.length()){
+        ans = max(ans,cal());
+        return;
+    }
+
+    for(int i = 1; i<=4; i++){
+        num[count] = i;
+        choose(count+1);
+    }
+}
+int main() {
+    // Please write your code here.
+    cin >> s;
+    for(int i = 0; i<s.length(); i++){
+        arr[i] = s[i];
+    }
+
+    choose(0);
+    cout << ans;
+
+    return 0;
+}
