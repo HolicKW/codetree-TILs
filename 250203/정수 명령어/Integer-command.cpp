055d405272a6bda2111fe0536efe08b1af047f52
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-03 17:26:13 +0900

    Title: 정수 명령어, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250203/정수 명령어/Integer-command.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int t,a;
+    set<int> s;
+    char c;
+    cin >> t;
+    for(int i = 0; i<t;i++){
+        int k;
+        cin >> k;
+        for(int j = 0; j<k;j++){
+            cin >> c;
+            if(c == 'I'){
+                cin >> a;
+                s.insert(a);
+            }
+            else if(c == 'D'){
+                cin>>a;
+                if(s.empty()){
+                    continue;
+                }
+                else if(a == 1){
+                    s.erase(*s.rbegin());
+                }
+                else if(a == -1){
+                    s.erase(*s.begin());
+                }
+            }
+        }
+        if(s.empty()){
+            cout << "EMPTY" << endl;
+        }
+        else{
+            cout << *s.rbegin() << " " << *s.begin() << endl;
+        }
+    }
+    return 0;
+}
