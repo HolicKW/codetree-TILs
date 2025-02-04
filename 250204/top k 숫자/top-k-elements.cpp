38584ac362f4963d6b716455b6cdf7ef23bce14f
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-04 13:11:52 +0900

    Title: top k 숫자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250204/top k 숫자/top-k-elements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int k,n;
+    cin >> n>> k;
+    set<int> s;
+    for(int i = 0; i<n;i++){
+        int num;
+        cin >> num;
+        s.insert(num);
+    }
+    auto it = s.rbegin();
+    for(int i = 0; i<k;i++){
+        cout << *it << " ";
+        it--;
+    }
+    return 0;
+}
