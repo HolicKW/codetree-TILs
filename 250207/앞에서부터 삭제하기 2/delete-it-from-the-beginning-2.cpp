6eb999d903f542efce41ada6944f4847e57f06d3
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-07 11:13:57 +0900

    Title: 앞에서부터 삭제하기 2, Time: 1075ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 250207/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+priority_queue<int,vector<int> ,greater<int>> pq;
+int arr[100000];
+int n;
+
+void set_pq(int k){
+    while(!pq.empty()){
+        pq.pop();
+    }
+    for(int i = k; i<n;i++){
+        pq.push(arr[i]);
+    }
+}
+double get_value(int n){
+    double sum = 0;
+    pq.pop();
+    for(int i = 0; i<n-1;i++){
+        sum += pq.top();
+        pq.pop();
+    }
+    return sum/(n-1);
+}
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i =0; i<n;i++){
+        cin >> arr[i];
+    }
+    double ans = 0;
+    for(int k = 1; k<=n-2;k++){
+        set_pq(k);
+        for(int t=0; t<k;t++){
+            pq.pop();
+        }
+
+        ans = max(ans,get_value(pq.size()));
+    }
+
+    printf("%.2f",ans);
+    return 0;
+}
