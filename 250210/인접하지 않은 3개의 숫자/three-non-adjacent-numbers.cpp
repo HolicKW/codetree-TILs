e387701ab815462cf9bf2496c30ee199964cd320
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-10 14:36:55 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250210/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
+++ 250210/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
@@ -7,26 +7,30 @@ int L[MAX+1];
 int R[MAX+1];
 
 int main() {
-    // Please write your code here.
+    // Input
     cin >> n;
-    for(int i = 1; i<=n;i++){
+    for(int i = 1; i<=n; i++){
         cin >> arr[i];
     }
 
+    // Calculate L array (left-to-right)
     L[1] = arr[1];
-    L[2] = max(arr[1],arr[2]);
+    L[2] = max(arr[1], arr[2]);
     for(int i = 3; i <= n; i++){
-        L[i] = max(L[i-1], L[i-2]+ arr[i]);
+        L[i] = max(L[i-1], L[i-2] + arr[i]);
     }
 
+    // Calculate R array (right-to-left)
     R[n] = arr[n];
-    R[n-1] = max(arr[n-1],arr[n]);
+    R[n-1] = max(arr[n-1], arr[n]);
     for(int i = n - 2; i >= 1; i--){
-        R[i] =max(R[i+1],R[i+2]+arr[i]);
+        R[i] = max(R[i+1], R[i+2] + arr[i]);
     }
-    int result= 0;
-    for(int i = 1; i<=n-2;i++){
-        result = max(result,L[i]+R[i+2]);
+
+    // Calculate the maximum sum of non-adjacent 3 elements
+    int result = 0;
+    for(int i = 1; i <= n-2; i++){
+        result = max(result, L[i] + R[i+2]);
     }
 
     cout << result;
