1994d05f66dc95523a03d1a2c98c5aeae41c1da1
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-15 19:51:45 +0900

    Title: 가장 짧은 부분합, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 250215/가장 짧은 부분합/shortest-subtotal.cpp
+++ 250215/가장 짧은 부분합/shortest-subtotal.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
 using namespace std;
+
 #define MAX 100000
+
 int main() {
-    // Please write your code here.
     int arr[MAX];
-    int n,s;
+    int n, s;
     cin >> n >> s;
-    int result = 100001;
-    for(int i = 1; i<=n;i++){
+    
+    for (int i = 0; i < n; i++) {  // 배열을 0부터 입력받도록 수정
         cin >> arr[i];
     }
 
-    int j = 1;
-    for(int i = 1; i<=n;i++){
-        int sum = 0;
-        while(j <= n && sum < s){
+    int result = MAX + 1;  // 최소 길이를 저장할 변수
+    int sum = 0, j = 0;    // `sum`과 `j`를 유지
+
+    for (int i = 0; i < n; i++) {
+        // j를 오른쪽으로 이동하며 sum을 증가
+        while (j < n && sum < s) {
             sum += arr[j];
             j++;
-            
         }
-        result = min(result, j-i+1);
 
+        // 조건을 만족하는 경우 최소 길이 업데이트
+        if (sum >= s) {
+            result = min(result, j - i);
+        }
+
+        // i를 증가시키면서 sum을 감소
         sum -= arr[i];
     }
 
-    
-
-    if(result == 100001){
+    if (result == MAX + 1) {
         cout << -1;
-    }
-    else{
+    } else {
         cout << result;
     }
+
     return 0;
 }
