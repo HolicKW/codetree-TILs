4a01d458b49a7c171c40895817d5f772dd57cf94
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-21 00:23:01 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250221/삼각형 만들기/create-triangle.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n;
+pair<int,int> arr[101];
+
+int triangle_area(int i, int j, int k){
+    int x1 = arr[i].first, y1 = arr[i].second;
+    int x2 = arr[j].first, y2 = arr[j].second;
+    int x3 = arr[k].first, y3 = arr[k].second;
+    return abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3));
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+
+    for(int i = 0; i<n;i++){
+        int x,y;
+        cin >> arr[i].first >> arr[i].second;
+
+    }
+    int ans = 0;
+    for(int i = 0; i<n;i++){
+        for(int j =i+1;j<n;j++){
+            for(int k = j+1; k<n;k++){
+                if(((arr[i].first == arr[j].first) || (arr[i].first == arr[k].first) ||  (arr[j].first == arr[k].first)) &&
+                ((arr[i].second == arr[j].second) || arr[i].second == arr[k].second) ||  (arr[j].second == arr[k].second)){
+                    ans = max(ans,triangle_area(i,j,k));
+                }
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
