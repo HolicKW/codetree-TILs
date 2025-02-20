a3625ef8782f9854b9d369d919e9e0361d54af97
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-20 23:44:21 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250220/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+using namespace std;
+int n;
+pair<int ,int>arr[101];
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i = 0; i<n;i++){
+        int x,y;
+        cin >> x >> y;
+        arr[i] = {x,y};
+    }
+    int ans = INT_MAX;
+    int min_x = INT_MAX;
+    int min_y = INT_MAX; 
+    for(int i = 0; i<n;i++){
+        for(int j = i+1; j<n;j++){
+            min_x = min(min_x,abs(arr[i].first - arr[j].first)); 
+            min_y = min(min_y,abs(arr[i].second - arr[j].second));
+            
+        }
+    }
+
+    cout << (min_x*min_x) + (min_y*min_y);
+    return 0;
+}
