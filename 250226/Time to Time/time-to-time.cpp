8f4127deebf14af28d1c1ae45021079518bee0da
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-26 19:29:32 +0900

    Title: Time to Time, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/Time to Time/time-to-time.cpp
+++ 250226/Time to Time/time-to-time.cpp
@@ -5,10 +5,19 @@ int main() {
     // Please write your code here.
     int a,b,c,d;
     cin >> a >> b >> c >> d;
+    int cnt = 0;
+    while(true){
+        if(a==c && b==d){
+            break;
+        }
+        b++;
+        cnt++;
+        if(b==60){
+            b=0;
+            a++;
+        }
+    }
 
-    int time_1 = a*60+b;
-    int time_2 = c*60+d;
-
-    cout << time_2-time_1;
+    cout << cnt;
     return 0;
 }
