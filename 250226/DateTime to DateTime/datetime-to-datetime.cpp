7b62093b329e67f16a12558366536c34be7d0739
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-02-26 20:26:24 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250226/DateTime to DateTime/datetime-to-datetime.cpp
@@ -5,15 +5,14 @@ int main() {
     // Please write your code here.
     int a,b,c;
     cin >> a >> b >> c;
-    if(a<11 && b<11 && c<11){
-        cout << -1;
-        return 0;
-    }
-
     int time = (a-11) * 1440;
     time += (b-11) * 60;
     time += c-11;
-
-    cout << time;
+    if(time >=0){
+        cout << time;
+    }
+    else{
+        cout << -1;
+    }
     return 0;
 }
