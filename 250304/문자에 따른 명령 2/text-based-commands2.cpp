ac98e09dce63f6c84dc05cce8994610a7c0a9cd6
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-04 18:21:10 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250304/문자에 따른 명령 2/text-based-commands2.cpp
+++ 250304/문자에 따른 명령 2/text-based-commands2.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int getdir(char dir){
-    if(dir == 'R'){
-        return 0;
-    }
-    else if(dir == 'D'){
-        return 1;
-    }
-    else if(dir == 'U'){
-        return 2;
-    }
-    else if(dir == 'L'){
-        return 3;
-    }
-}
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
-bool inrange(int x, int y, int ran){
-    return(x>=1 && x<=ran && y>=1 && y<=ran);
-}
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n,t;
-    cin >> n >> t;
-    int arr[n][n];
-    int dx[4] = {1,0,0,-1};
-    int dy[4] = {0,1,-1,0};
-    int r,c;
-    char d;
-    cin >> r >> c >> d;
-    int dir = getdir(d);
-    for(int i = 0; i<t;i++){
-        int ny = r + dy[dir];
-        int nx = c + dx[dir];
-        if(!inrange(nx,ny,n)){
-            dir = 3-dir;
-            continue;
+    // Please write your code here.
+    string com;
+    int x = 0, y= 0;
+    cin >> com;
+    int dir = 0;
+    for(int i = 0; i<com.length(); i++){
+        if(com[i] == 'L'){
+            dir = (dir+3)%4;
         }
-        r = r + dy[dir];
-        c = c + dx[dir];
+        else if (com[i] == 'R'){
+            dir = (dir+1)% 4;
+        }
+        else{
+            x += dx[dir];
+            y += dy[dir];
+        }
     }
 
-    cout << r << ' ' << c;
+    cout << x << ' ' << y;
     return 0;
 }
