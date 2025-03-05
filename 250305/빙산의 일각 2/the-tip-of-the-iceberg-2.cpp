1dbf17f6fe9750fa6cf1cf74597b5e20cf0f5629
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-05 13:45:34 +0900

    Title: 빙산의 일각 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250305/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[101];
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int max_height = 0;
+    for(int i = 0; i<n;i++){
+        cin >> arr[i];
+        max_height = max(max_height,arr[i]);
+    }
+    int ans = 0;
+    for(int i = 1; i<max_height; i++){
+        int cnt = 0;
+        for(int j = 0; j<n;j++){
+            if(arr[j]-i >0 && arr[j+1] - i <= 0){
+                cnt++;
+            }
+        }
+        ans = max(ans,cnt);
+    }
+
+    cout << ans;
+    return 0;
+}
