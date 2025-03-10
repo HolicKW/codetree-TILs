669f1e57f0fb5252ca0720ea924858f07c7af6b2
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-10 18:59:47 +0900

    Title: 독서실의 거리두기 4, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
+++ 250310/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -16,11 +16,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         if (str[i] == '1') continue;
         str[i] = '1'; 
-        int min_dis = INT_MAX;
         for(int j =i+1; j<n;j++){
             if(str[j] == '1') continue;
-            str[j] == '1';
-
+            str[j] = '1';
+            int min_dis = INT_MAX;
             for (int k = 0; k < n; k++) {
                 if (str[k] == '1') {
                     for (int t = k + 1; t < n; t++) {
@@ -31,10 +30,10 @@ int main() {
                     }
                 }
             }
+            if (min_dis != INT_MAX) {
+                ans = max(ans, min_dis);
+            }
             str[j] = '0';
-        }
-        if (min_dis != INT_MAX) {
-            ans = max(ans, min_dis);
         }
     
         
