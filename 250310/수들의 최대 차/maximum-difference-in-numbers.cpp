ca397ef286031997af015e00604c3822c079af56
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-10 15:21:30 +0900

    Title: 수들의 최대 차, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/수들의 최대 차/maximum-difference-in-numbers.cpp
+++ 250310/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -15,7 +15,7 @@ int main() {
     for(int i = 0; i<n;i++){
         int cnt = 0;
         for(int j = 0; j<n;j++){
-            if(arr[j] <= arr[i]+k && arr[j]>=i){
+            if(arr[j] <= arr[i]+k && arr[j]>=arr[i]){
                 cnt++;
             }
         }
