df47456196d52d1658cb517b6e9f8d4a1328753e
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-10 12:46:44 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 250310/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -11,32 +11,25 @@ int main() {
     cin >> n;
     cin >> str;
 
-    int ans = 0; // 초기값을 0으로 설정
+    int ans = 0; 
 
     for (int i = 0; i < n; i++) {
-        if (str[i] == '1') continue; // 원래 '1'이면 건너뜀
-
+        if (str[i] == '1') continue;
         str[i] = '1'; 
-        int max_min = 21; 
-
+        int min_dis = INT_MAX;
         for (int j = 0; j < n; j++) {
             if (str[j] == '1') {
-                int min_dis = INT_MAX;
-
                 for (int k = j + 1; k < n; k++) {
                     if (str[k] == '1') {
                         min_dis = min(min_dis, k - j);
-                        break; // 가장 가까운 거리만 찾으면 되므로 break
+                        break; 
                     }
-                }
-
-                if (min_dis != INT_MAX) {
-                    max_min = min(max_min, min_dis);
                 }
             }
         }
-
-        ans = max(ans, max_min);
+        if (min_dis != INT_MAX) {
+            ans = max(ans, min_dis);
+        }
         str[i] = '0'; // 원상 복구
     }
 
