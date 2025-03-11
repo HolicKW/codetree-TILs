6551d7d9c72a3f3d9d974b0fe4448d9fc3093f05
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-11 18:57:12 +0900

    Title: 이상한 폭탄 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250311/이상한 폭탄 3/strange-bomb-3.cpp
+++ 250311/이상한 폭탄 3/strange-bomb-3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int arr[101];
+int bomb[101];
+bool explode[101];
 int main() {
     // Please write your code here.
     int n,k;
@@ -9,25 +11,31 @@ int main() {
     for(int i = 0; i<n;i++){
         cin >> arr[i];
     }
-    int ans = 0;
-    int sum = 0;
+    int ans = 1;
+    int idx = 0;
     for(int i = 0; i<n;i++){
-        int cnt = 0;
-        int prev = i;
         for(int j = i+1; j<n;j++){
-            if(arr[i] == arr[j] && j-prev <= k){
-                cnt++;
-            }
-            prev = j;
-        }
-        if(cnt>=sum && cnt != 0){
-            sum = cnt;
-            ans = arr[i];
-            
-        }
+            if(j-i>k) break;
 
+            if(arr[i] != arr[j])continue;
+
+            if(explode[i] == false){
+                explode[i] = true;
+                bomb[i]++;
+            }
+            if(explode[j] == false){
+                explode[j] = true;
+                bomb[j]++;
+            }
+        }
+    }
+    for(int i = 0; i<n;i++){
+        if(bomb[i] >= ans){
+            ans = bomb[i];
+            idx = i;
+        }
     }
 
-    cout << ans;
+    cout << idx;
     return 0;
 }
