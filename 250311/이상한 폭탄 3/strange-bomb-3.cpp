45eff08bbd0077f4c727f4763631a8c14d5ee73b
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-11 18:07:33 +0900

    Title: 이상한 폭탄 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250311/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int arr[101];
+int main() {
+    // Please write your code here.
+    int n,k;
+    cin >> n >> k;
+
+    for(int i = 0; i<n;i++){
+        cin >> arr[i];
+    }
+    int ans = 0;
+    int sum = 0;
+    for(int i = 0; i<n;i++){
+        int cnt = 0;
+        int prev = 0;
+        for(int j = i+1; j<n;j++){
+            if(arr[i] == arr[j] && j-prev <= k){
+                cnt++;
+                prev = j;
+            }
+
+        }
+        if(cnt>=sum){
+            sum = cnt;
+            ans = arr[i];
+        }
+
+    }
+
+    cout << ans;
+    return 0;
+}
