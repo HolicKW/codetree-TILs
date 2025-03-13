5af68311b2ed4f85801b4ef065ae46f5420a7c5e
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-13 21:18:34 +0900

    Title: 구간 잘 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250313/구간 잘 나누기/divide-sections-well.cpp
+++ 250313/구간 잘 나누기/divide-sections-well.cpp
@@ -9,26 +9,30 @@ int main() {
     for(int i = 0; i<n;i++){
         cin >> arr[i];
     }
-
+    int ans = 100001;
     for(int i = 1; i<10001;i++){
         int sum = 0;
-        int mcnt = 0;
+        int mcnt = 1;
+        bool pos = true;
         for(int j = 0; j<n; j++){
             if(arr[j] > i){
+                pos = false;
                 break;
             }
+
             sum += arr[j];
+
             if(sum > i){
                 sum = arr[j];
                 mcnt++;
             }
 
-            if((mcnt == m-1 && j == n-1) ||(mcnt == m-2 &&  j == n-1 && sum <= i)){
-                cout << i;
-                return 0;
-            }
+        }
+        if((pos && mcnt <= m)){
+            ans = min(ans,i);
         }
 
     }
+    cout << ans;
     return 0;
 }
