258aedb21d573933ed794043cc7ea6500e6e1f12
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-13 21:30:23 +0900

    Title: 두 선분, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250313/두 선분/two-lines.cpp
+++ 250313/두 선분/two-lines.cpp
@@ -6,7 +6,7 @@ int x1,x2,x3,x4;
 int main() {
     // Please write your code here.
     cin >> x1 >> x2 >> x3 >> x4;
-    if(x2 > x3 || x1 > x4){
+    if(x2 >= x3 || x1 >= x4){
         cout << "intersecting";
     }
     else{
