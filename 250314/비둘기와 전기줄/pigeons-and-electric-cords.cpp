53448c45bc9df6bc8a389466337010d4369a1740
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-14 00:09:22 +0900

    Title: 비둘기와 전기줄, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250314/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<vector<int>> vec(11);
+int main() {
+    // Please write your code here.
+    int n;
+    int num,dir;
+    cin >> n;
+    for(int i = 0; i<n;i++){
+        cin >> num >> dir;
+        vec[num].push_back(dir);
+    }
+    int cnt = 0;
+    for(int i = 0; i<10;i++){
+        if(vec[i].size() < 2){
+                continue;
+        }
+        for(int j = 0; j<vec[i].size()-1;j++){
+            if((vec[i][j] == 1 && vec[i][j+1] == 0) || (vec[i][j] == 0 && vec[i][j+1] == 1)){
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
