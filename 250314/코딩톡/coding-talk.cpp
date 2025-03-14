da35bd4d3af1d132268be9444aeba4bc85968dd5
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-14 17:11:44 +0900

    Title: 코딩톡, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250314/코딩톡/coding-talk.cpp
+++ 250314/코딩톡/coding-talk.cpp
@@ -7,12 +7,24 @@ int main() {
     // Please write your code here.
     int n,m,p,u;
     char c;
+    int cnt = m;
     cin >> n >> m  >> p;
     for(int i = 0; i<n; i++){
         s.insert('A'+ i);
     }
     for(int i = 0; i<m;i++){
         cin >> arr[i].first >> arr[i].second;
+    }
+    if(arr[p-1].second == 0){
+        return 0;
+    }
+    
+    for(int i = 0; i<p-1;i++){
+        if(arr[i].second == arr[p-1].second){
+            if(s.find(arr[i].first) !=s.end()){
+            s.erase(arr[i].first);
+            }
+        }
     }
     for(int i = p-1; i<m;i++){
         if(s.find(arr[i].first) !=s.end()){
