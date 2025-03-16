54a3d130c56db2836638cf2736ab532e1ab72af1
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-16 17:38:23 +0900

    Title: N개의 점 중 M개 고르기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250316/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
+++ 250316/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
@@ -10,38 +10,41 @@ vector<pair<int,int>> vec;
 int ans = INT_MAX;
 
 int n,m;
-
 int dist(vector<pair<int,int>> &dir){
-    int min_x = INT_MAX, min_y = INT_MAX;
-    int max_x = 0, max_y = 0;
+    int dis = 0;
+    int sum = 0;
     for(int i = 0; i<dir.size(); i++){
-        min_x = min(min_x,dir[i].first);
-        min_y = min(min_y,dir[i].second);
-        max_x = max(max_x,dir[i].first);
-        max_y = max(max_y,dir[i].second);
+        for(int j = i+1; j<dir.size();j++){
+            int x1 = dir[i].first, y1 = dir[i].second;
+            int x2 = dir[j].first, y2 = dir[j].second;
+            sum = pow(x1 - x2, 2) + pow(y1 - y2, 2);
+            dis = max(dis,sum);
+        }
     }
-    int sum = pow(max_x-min_x,2) + pow(max_y-min_y,2);
-    return sum;
+    return dis;
 }
+
 void sol(int cnt, int idx){
     if(cnt == m){
-        ans = min(ans,dist(vec));
+        ans = min(ans, dist(vec));
         return;
     }
 
     for(int i = idx; i<n; i++){
         vec.push_back(arr[i]);
-        sol(cnt+1,i+1);
+        sol(cnt+1, i+1);
         vec.pop_back();
     }
 }
+
 int main() {
-    // Please write your code here.
     cin >> n >> m;
-    for(int i = 0; i<n;i++){
+    for(int i = 0; i < n; i++){
         cin >> arr[i].first >> arr[i].second;
     }
-    sol(0,0);
+
+    sol(0, 0);
+
     cout << ans;
     return 0;
 }
