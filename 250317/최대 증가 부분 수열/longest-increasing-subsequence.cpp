bc6d987f50e51206a9bb6cd8e31859b9773ed303
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-17 20:46:57 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 250317/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 250317/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 #define MAX 1000
 int arr[MAX];
@@ -10,11 +11,11 @@ int main() {
     for(int i = 0; i<n;i++){
         cin >> arr[i];
     }
-    dp[0] = 1;
+    for(int i =0; i<n;i++){
+        dp[i] = 1; 
+    }
     for(int i = 1; i<n;i++){
         for(int j = 0; j<i;j++){
-            if(dp[j] == 0) continue;
-
             if(arr[i] > arr[j]){
                 dp[i] = max(dp[i],dp[j]+1);
             }
