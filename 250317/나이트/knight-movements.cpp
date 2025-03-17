b1e4348dbba9a7c4e1da02475e0f763f57f8b1c4
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-17 19:39:04 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250317/나이트/knight-movements.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+#define MAX 100
+using namespace std;
+
+int arr[MAX][MAX];
+bool visited[MAX][MAX];
+int r1,c1,r2,c2;
+queue<pair<int,int>> q;
+int n;
+int dx[8] = {-2,-1,1,2,2,1,-1,-2};
+int dy[8] = {1,2,2,1,-1,-2,-2,-1};
+
+bool inRange(int x, int y){
+    return x>=0 && x<n && y>=0 && y<n;
+}
+
+bool canGo(int x, int y){
+    return inRange(x,y) && !visited[x][y];
+}
+void bfs(){
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int i = 0; i<8; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(canGo(nx,ny)){
+                q.push({nx,ny});
+                arr[nx][ny] = arr[x][y]+1;
+                visited[nx][ny] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    cin >> r1 >> c1 >> r2 >> c2;
+    r1--; c1--; r2--; c2--;
+    q.push({r1,c1});
+    bfs();
+    cout << arr[r2][c2];
+    return 0;
+}
