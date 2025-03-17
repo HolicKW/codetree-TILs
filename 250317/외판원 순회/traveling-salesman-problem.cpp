7a42ff4b9f33df1b514b383127d3e2651b54e4fb
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-17 14:15:25 +0900

    Title: 외판원 순회, Time: 46ms, Memory: 0MB, Status: Passed - CodeTree

--- 250317/외판원 순회/traveling-salesman-problem.cpp
+++ 250317/외판원 순회/traveling-salesman-problem.cpp
@@ -3,45 +3,50 @@
 #include <climits>
 #define MAX 10
 using namespace std;
+
 int n;
 int arr[MAX][MAX];
 bool visited[MAX];
 vector<int> vec;
-
 int ans = INT_MAX;
 
-int calc(){
+int calc() {
     int sum = 0;
-    for(int i = 0; i<vec.size();i++){
-        sum += arr[i][vec[i]];
+    for (int i = 0; i < vec.size() - 1; i++) {
+        sum += arr[vec[i]][vec[i + 1]];
     }
+    sum += arr[vec.back()][vec[0]]; // 마지막 도시에서 출발 도시로 복귀하는 비용 추가
     return sum;
 }
-void sol(int cnt){
-    if(cnt == n){
-        ans = min(ans,calc());
+
+void sol(int city, int cnt) {   //city 에서 i번째 도시를 방문
+    if (cnt == n) {
+        if (arr[city][0] == 0) return; 
+        ans = min(ans, calc());
         return;
     }
 
-    for(int i = 0; i<n;i++){
-        if(visited[i] || arr[cnt][i] == 0) continue;
+    for (int i = 0; i < n; i++) {
+        if (visited[i] || arr[city][i] == 0) continue;
         visited[i] = true;
         vec.push_back(i);
-        sol(cnt+1);
+        sol(i, cnt + 1);
         vec.pop_back();
         visited[i] = false;
     }
-
 }
+
 int main() {
-    // Please write your code here.
     cin >> n;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n;j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
         }
     }
-    sol(0);
+
+    visited[0] = true; 
+    vec.push_back(0);
+    sol(0, 1);
     cout << ans;
     return 0;
 }
