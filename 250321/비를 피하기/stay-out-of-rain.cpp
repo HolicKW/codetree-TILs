bfefcb661335a5db218fc050fbe56bd730593532
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-21 14:33:12 +0900

    Title: 비를 피하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 250321/비를 피하기/stay-out-of-rain.cpp
+++ 250321/비를 피하기/stay-out-of-rain.cpp
@@ -23,31 +23,19 @@ bool canGo(int x,int y){
     return isRange(x,y) && !visited[x][y] && arr[x][y] != 1;
 }
 
-void init(){
-    for(int i = 0; i<n;i++){
-        for(int j = 0; j<n;j++){
-            visited[i][j] = 0;
-            rain[i][j] = 0;
-        }
-    }
-}
-
-void bfs(int startX, int startY){
-    init();
-    visited[startX][startY] = 1;
-    q.push({startX, startY});
-    
+void bfs(){
     while(!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
+        
         for(int i = 0; i<4;i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(canGo(nx,ny)){
                 rain[nx][ny] = rain[x][y] + 1;
-                if(arr[nx][ny] == 3){
-                    result[startX][startY] = min(result[startX][startY], rain[nx][ny]);
+                if(arr[nx][ny] == 2){
+                    result[nx][ny] = min(result[nx][ny], rain[nx][ny]);
                 }
                 visited[nx][ny] = 1;
                 q.push({nx,ny});
@@ -59,6 +47,8 @@ void bfs(int startX, int startY){
 
 int main(){
     cin >> n >> h >> m;
+    
+    // 초기화
     for(int i = 0; i<n;i++){
         for(int j = 0; j<n;j++){
             cin >> arr[i][j];
@@ -68,14 +58,19 @@ int main(){
         }
     }
     
-    // 각 사람(2)의 위치에서 BFS 시작
+    // 모든 비를 피할 수 있는 곳(3)에서 동시에 BFS 시작
     for(int i = 0; i<n;i++){
         for(int j = 0; j<n;j++){
-            if(arr[i][j] == 2){
-                bfs(i,j);
+            if(arr[i][j] == 3){
+                visited[i][j] = 1;
+                q.push({i,j});
             }
         }
     }
+    
+    bfs();
+    
+    // 결과 출력
     for(int i = 0; i<n;i++){
         for(int j = 0; j<n;j++){
             if(arr[i][j] == 2 && result[i][j] == INT_MAX){
