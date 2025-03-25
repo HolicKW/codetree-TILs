d533b356f1736f494af764c82709618f761e473f
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-03-25 14:40:54 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250325/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n,m;
+int coin[100];
+int dp[10001];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> coin[i];
+    }
+    sort(coin,coin+n);
+    dp[0] = 0;
+
+    for(int i = 1; i<=m; i++){
+        dp[i] = INT_MAX;
+        for(int j = 0; j<n;j++){
+            if(i >= coin[j]){
+                dp[i] = min(dp[i],dp[i-coin[j]]+1);
+            }
+        }
+    }
+    cout << dp[m];
+    // Please write your code here.
+
+    return 0;
+}
