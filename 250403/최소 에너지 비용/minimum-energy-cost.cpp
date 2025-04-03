78950dae7d6ef887a06d9580d168fbea736295d4
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-04-03 16:13:39 +0900

    Title: 최소 에너지 비용, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250403/최소 에너지 비용/minimum-energy-cost.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int n;
+int arr[100001];
+int dis[100000];
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i = 0; i<n-1; i++){ 
+        cin >> dis[i];
+    }
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    int sum = 0;
+    int min_cost = INT_MAX;
+    for(int i = 0; i<n-1;i++){
+        min_cost = min(min_cost,arr[i]);
+        sum += min_cost * dis[i];
+    }
+    cout << sum;
+    return 0;
+}
