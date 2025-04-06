70c7c0637a8ee7d4681480e7e7fdade8e3935241
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-04-06 12:27:55 +0900

    Title: 선분 위의 점, Time: 21ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250406/선분 위의 점/point-on-the-line-segment.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <set>
+using namespace std;
+#define MAX 100000
+int arr[MAX];
+int num[MAX];
+
+int n,m;
+int lower_b(int target){
+    int left = 0;
+    int right = n-1;
+    int mid_idx = n;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(arr[mid]>=target){
+            right = mid - 1;
+            mid_idx = min(mid_idx,mid);
+        }
+        else{
+            left = mid+1;
+        }
+    }
+    return mid_idx;
+}
+int upper_b(int target){
+    int left = 0;
+    int right = n-1;
+    int mid_idx = n;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(arr[mid]>target){
+            right = mid - 1;
+            mid_idx = min(mid_idx,mid);
+        }
+        else{
+            left = mid+1;
+        }
+    }
+    return mid_idx;
+
+}
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+    for(int i = 0; i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i<m;i++){
+        int a,b;
+        cin >> a >> b;
+        cout << upper_b(b)-lower_b(a) << endl;
+    }
+    return 0;
+}
