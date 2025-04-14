10ba43af8a8e8796786d46a977683d83e76abbff
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-04-15 08:54:51 +0900

    Title: 수 채우기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250415/수 채우기/fill-in-number.cpp
+++ 250415/수 채우기/fill-in-number.cpp
@@ -5,14 +5,23 @@ int main() {
     // Please write your code here.
     int n;
     cin >> n;
+    if(n == 1 || n == 3){
+        cout << -1;
+        return 0;
+    }
     int cnt = 0;
     cnt += n/5;
-    n =  n%5;
+    n = n%5;
     if(n%2 !=0){
         n+=5;
         cnt--;
     }
     cnt += n/2;
+    n = n%2;
+    if(n != 0){
+        cout << -1;
+        return 0;
+    } 
     cout << cnt;
 
 
