0f77a4c7c6e33a28b8089b068660931a3f790c59
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-04-18 14:40:08 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250418/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <climits>
+using namespace std;
+#define MAX 100000
+
+int n, m;
+pair<long long, long long> line[MAX];
+
+bool isPossible(long long mid){
+    long long count = 0;
+    for(int i = 0; i < m; i++){
+        long long start = line[i].first;
+        long long end = line[i].second;
+
+        long long pos = start;
+        while (pos <= end) {
+            count++;
+            if (count >= n) return true;
+            pos += mid;
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        cin >> line[i].first >> line[i].second;
+    }
+
+    sort(line, line + m);
+    long long left = 1;
+    long long right = 1e18;
+    long long ans = 0;
+
+    while (left <= right){
+        long long mid = (left + right) / 2;
+        if (isPossible(mid)){
+            ans = max(ans, mid);
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
