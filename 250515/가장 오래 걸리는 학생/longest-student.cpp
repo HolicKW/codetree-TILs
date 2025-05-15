b05ba2f0d2af644fb663c9978f8ef280bb5b5962
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-05-15 14:58:38 +0900

    Title: 가장 오래 걸리는 학생, Time: 157ms, Memory: 5MB, Status: Passed - Codetree

--- 250515/가장 오래 걸리는 학생/longest-student.cpp
+++ 250515/가장 오래 걸리는 학생/longest-student.cpp
@@ -7,17 +7,13 @@ using namespace std;
 
 int n, m;
 vector<vector<pair<int,int>>> graph;
-int ans = 0;
 int dist[100001];
-void dijkstra(int v){
 
+void dijkstra(int start){
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    
     for (int i = 1; i <= n; i++) dist[i] = INT_MAX;
-
-    dist[v] = 0;
-
-    pq.push({0,v});
+    dist[start] = 0;
+    pq.push({0, start});
 
     while(!pq.empty()){
         int Curweight = pq.top().first;
@@ -29,31 +25,32 @@ void dijkstra(int v){
         for(auto edge : graph[Curnode]){
             int weight = edge.first;
             int next = edge.second;
-            if(dist[next] > Curweight+weight){
-                dist[next] = Curweight+weight;
-                pq.push({dist[next],next});
+            if(dist[next] > Curweight + weight){
+                dist[next] = Curweight + weight;
+                pq.push({dist[next], next});
             }
         }
-        
     }
-    
-    
 }
-int main() {
-    // Please write your code here.
-    cin >> n >> m;
-    graph.resize(n+1);
-    for(int i = 0; i<m; i++){
-        int start,end,w;
-        cin >> start >> end >> w;
-        graph[start].push_back({w,end});
-        graph[end].push_back({w,start});
-    }
-    for(int i = 1; i<n;i++){
-        dijkstra(i);
-        ans = max(ans,dist[n]);
-    }
-    cout << ans;
 
+int main() {
+    cin >> n >> m;
+    graph.resize(n + 1);
+    for(int i = 0; i < m; i++){
+        int start, end, w;
+        cin >> start >> end >> w;
+        graph[end].push_back({w, start}); // 방향 반대로 저장
+        graph[start].push_back({w, end}); // 양방향인 경우 유지
+    }
+
+    dijkstra(n); // n번 노드에서 출발하여 모든 정점까지 거리 계산
+
+    int ans = 0;
+    for (int i = 1; i < n; i++){
+        if (dist[i] != INT_MAX)
+            ans = max(ans, dist[i]);
+    }
+
+    cout << ans;
     return 0;
 }
