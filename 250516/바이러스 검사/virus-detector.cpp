89dd7b9e0bc844acc4c33555e04239a1ca9b6c9a
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-05-16 16:50:05 +0900

    Title: 바이러스 검사, Time: 458ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 250516/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int cus[1000001];
+int team1,team2;
+int check(int idx){
+    int r = cus[idx] - team1;
+    if(r<0) return 0;
+    if(r%team2 == 0){
+        return r/team2;
+    }
+    else{
+        return (r/team2)+1;
+    }
+
+}
+int main() {
+    // Please write your code here.
+
+    cin >> n;
+    for(int i = 0; i<n;i++){
+        cin >> cus[i];
+    }
+    cin >> team1 >> team2;
+    int cnt = 0;
+    for(int i = 0; i<n;i++){
+        cnt += (1+check(i));
+    }
+
+    cout << cnt;
+    return 0;
+}
