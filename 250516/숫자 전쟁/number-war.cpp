6dfac83a097904d80e62e0a54ddd3188bd674f29
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-05-16 15:57:52 +0900

    Title: 숫자 전쟁, Time: 9ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 250516/숫자 전쟁/number-war.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1001][1001];
+int card_one[1001];
+int card_two[1001];
+int n;
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> card_one[i];
+    }
+    for(int i = 1; i <= n; i++){
+        cin >> card_two[i];
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            // 1. 왼쪽만 버릴 수 있음
+            dp[i][j] = max(dp[i][j], dp[i-1][j]);
+            // 2. 오른쪽만 버리고 점수 획득
+            if(card_one[i] > card_two[j]) {
+                dp[i][j] = max(dp[i][j], dp[i][j-1] + card_two[j]);
+            }
+            // 3. 양쪽 모두 버릴 수 있음
+            if(card_one[i] == card_two[j]) {
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1]);
+            }
+        }
+    }
+    cout << dp[n][n] << endl;
+    return 0;
+}
