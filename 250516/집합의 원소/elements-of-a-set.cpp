7d7f85a438baf84c260d26a2d93625106247eae3
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-05-16 16:28:18 +0900

    Title: 집합의 원소, Time: 110ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250516/집합의 원소/elements-of-a-set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int uf[100001];
+
+
+
+int Find(int n){
+    if(uf[n] == n) return n;
+    int r= Find(uf[n]);
+    uf[n] = r;
+    return r;
+}
+void Union(int a, int b){
+    int x = Find(a);
+    int y = Find(b);
+    uf[x]=y;
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+    for(int i = 1; i<=n;i++){
+        uf[i] = i;
+    }
+    for(int i = 0; i<m;i++){
+        int num, a, b;
+        cin >> num >> a >> b;
+        if(num == 0){
+            Union(a,b);
+        }
+        else{
+            if(uf[a] == uf[b]){
+                cout << 1 << endl;
+            }
+            else{
+                cout << 0 << endl; 
+            }
+        }
+    }
+    return 0;
+}
