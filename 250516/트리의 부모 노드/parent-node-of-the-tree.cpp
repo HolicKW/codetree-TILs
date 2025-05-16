b0eed8a685988237437346beb937a94e66804e7a
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-05-16 14:44:46 +0900

    Title: 트리의 부모 노드, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250516/트리의 부모 노드/parent-node-of-the-tree.cpp
+++ 250516/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 using namespace std;
 
+vector<vector<int>> graph;
 int main() {
     int n;
     cin >> n;
-    vector<int> parent(n+1); // parent[i]: i번 노드의 부모
-
-    for(int i = 2; i <= n; i++){
-        int p, c;
+    graph.resize(n+1);
+    for(int i = 0;i<n;i++){
+        int p,c;
         cin >> p >> c;
-        parent[c] = p; // c번 노드의 부모는 p
+        graph[p].push_back(c);
+        graph[c].push_back(p);
     }
-
-    for(int i = 2; i <= n; i++){
-        cout << parent[i] << endl;
-    }
+    for(int i = 2;i<=n;i++){
+        cout << graph[i][0]<<endl;
+    }   
     return 0;
 }
