8da0209f2d9b85d72b5d966863d0640162f12cc4
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-07-19 20:07:51 +0900

    Title: 숫자들의 합이 7의 배수, Time: 382ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 250719/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int numbers[50000];
+int prefix[50000];
+
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    prefix[0] = numbers[0];
+    for(int i = 1;i<n;i++){
+        prefix[i] = prefix[i-1]+numbers[i];
+    }
+    int ans = 0;
+    for(int i = 0; i<n;i++){
+        for(int j = 0; j<i; j++){
+            if((prefix[i]-prefix[j]) % 7 == 0){
+                ans = max(ans,i-j);
+                break;
+            }
+        }
+    }
+
+    cout << ans;
+
+
+    // Please write your code here.
+
+    return 0;
+}
